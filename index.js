d6bafd9cfea3ebe708b520130130fe56b24addc3
Juan Camilo Salazar Quintero <jc.salazarquintero@gmail.com>
2024-09-01 23:54:07 -0500

    1

---
+++ index.js
@@ -0,0 +1,25 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const users_router_1 = require("./routes/users.router");
+const comment_router_1 = require("./routes/comment.router");
+const db_1 = require("./config/db");
+const reaction_router_1 = require("./routes/reaction.router");
+const app = (0, express_1.default)();
+dotenv_1.default.config();
+const PORT = process.env.PORT || 8000;
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use('/api/users', users_router_1.router);
+app.use('/api/comments', comment_router_1.routerComment);
+app.use('/api/reactions', reaction_router_1.routerReactions);
+app.get('/', (req, res) => {
+    res.send('AAAAAAAAAAAAAAAAAAAAa');
+});
+db_1.db.then(() => app.listen(PORT, () => {
+    console.log(`server running on http://localhost:${PORT}`);
+}));
